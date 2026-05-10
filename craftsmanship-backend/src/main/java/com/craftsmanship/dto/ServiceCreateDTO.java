b66c4206fde8685d109c49b11055ab6d039ff5a3
Abdullah Farwaneh <abdullahfarwaneh700@gmail.com>
2026-05-10 17:16:49 +0300

    Update ServiceCreateDTO.java

--- craftsmanship-backend/src/main/java/com/craftsmanship/dto/ServiceCreateDTO.java
+++ craftsmanship-backend/src/main/java/com/craftsmanship/dto/ServiceCreateDTO.java
@@ -3,10 +3,10 @@ import lombok.Data;
 import java.util.List;
 @Data
 public class ServiceCreateDTO {
-    private String serviceName;        // frontend sends "serviceName"
-    private String description;
+    private String     serviceName;        // frontend sends "serviceName"
+    private String   description;
     private String phoneNumber;
-    private String serviceType;
+    private String  serviceType;
     private String address;
     private List<String> availableDays; // frontend sends string[]
     private String availableTimeFrom;
